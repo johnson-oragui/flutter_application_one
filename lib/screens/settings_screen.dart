32aedd9eb632f229d95fb46d56ad2e703e6c9e85
johnson-oragui <johnson.oragui@gmail.com>
2025-05-14 18:54:39 +0100

    feat: updated navigation based on logged in user  - added means to toggle clear email on logout  - added means to toggle enable login with fingerprint

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -11,6 +11,41 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   int _selectedIndex = 0;
+  bool? _isLoggedIn;
+  bool _fingerprintEnabled = false;
+  bool? _clearEmailOnLogout;
+
+  @override
+  void initState() {
+    super.initState();
+
+    authService.value.checkIsLoggedIn().then((isIn) {
+      setState(() {
+        _isLoggedIn = isIn;
+      });
+      debugPrint("during check _isLoggedIn $_isLoggedIn");
+    });
+
+    authService.value.getSavedEmailOnLogoutOption().then((bool? value) {
+      if (value == null) {
+        setState(() {
+          _clearEmailOnLogout = false;
+        });
+      } else {
+        setState(() {
+          _clearEmailOnLogout = value;
+        });
+      }
+    });
+
+    authService.value.getFingerprintLoginOption().then((bool? value) {
+      if (value != null) {
+        setState(() {
+          _fingerprintEnabled = value;
+        });
+      }
+    });
+  }
 
   Future<void> _handleNavigation(int index) async {
     // Ensure index is within valid range
@@ -27,7 +62,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         Navigator.pushReplacementNamed(context, '/home');
         break;
       case 3:
-        await AuthService.logoutUser();
+        await authService.value.logoutUser();
         if (context.mounted) {
           Navigator.pushReplacementNamed(context, '/login');
         }
@@ -45,115 +80,134 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     final isWide = MediaQuery.of(context).size.width >= 600;
 
-    bool isLoggedIn = false;
-
-    AuthService.checkIsLoggedIn().then((isIn) {
-      isLoggedIn = isIn;
-    });
-
-    // wide screen
-    final destinations = [
-      NavigationRailDestination(
-        icon: Icon(Icons.dashboard_customize_sharp),
-        label: Text('Dashboard'),
-      ),
-      NavigationRailDestination(
-        icon: Icon(Icons.person),
-        label: Text('Profile'),
-      ),
-      NavigationRailDestination(icon: Icon(Icons.home), label: Text('Home')),
-    ];
-
-    // not wide screen
-    List<Widget> children = [
-      const DrawerHeader(
-        decoration: BoxDecoration(color: Colors.blue),
-        margin: EdgeInsets.only(bottom: 5.0),
-        padding: EdgeInsets.only(bottom: 10.0),
-        child: Text('Menu', style: TextStyle(color: Colors.white)),
-      ),
-      ListTile(
-        leading: const Icon(Icons.dashboard_customize_sharp),
-        title: const Text('Dashboard'),
-        onTap: () => _handleNavigation(0),
-      ),
-      ListTile(
-        leading: const Icon(Icons.person),
-        title: const Text('Profile'),
-        onTap: () => _handleNavigation(1),
-      ),
-      ListTile(
-        leading: const Icon(Icons.home),
-        title: const Text('Home'),
-        onTap: () => _handleNavigation(2),
-      ),
-    ];
-
-    // conditionally add icons if user is logged in
-    if (isLoggedIn) {
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.logout_sharp),
-          label: Text('Logout'),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.logout_sharp),
-          title: const Text('Logout'),
-          onTap: () => _handleNavigation(3),
-        ),
-      );
-    }
-    if (!isLoggedIn) {
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.login_sharp),
-          label: Text('Login'),
-        ),
-      );
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.app_registration_sharp),
-          label: Text('Register'),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.login_sharp),
-          title: const Text('Login'),
-          onTap: () => _handleNavigation(4),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.app_registration_sharp),
-          title: const Text('Register'),
-          onTap: () => _handleNavigation(5),
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
       drawer:
           isWide
               ? null
-              : Drawer(width: 160.0, child: ListView(children: children)),
+              : Drawer(
+                width: 160.0,
+                child: ListView(
+                  children: [
+                    const DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.blue),
+                      margin: EdgeInsets.only(bottom: 5.0),
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        'Menu',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.dashboard_customize_sharp),
+                      title: const Text('Dashboard'),
+                      onTap: () => _handleNavigation(0),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text('Profile'),
+                      onTap: () => _handleNavigation(1),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.home),
+                      title: const Text('Home'),
+                      onTap: () => _handleNavigation(2),
+                    ),
+                    if (_isLoggedIn == null || _isLoggedIn == false) ...[
+                      ListTile(
+                        leading: const Icon(Icons.login_sharp),
+                        title: const Text('Login'),
+                        onTap: () => _handleNavigation(4),
+                      ),
+
+                      ListTile(
+                        leading: const Icon(Icons.app_registration_sharp),
+                        title: const Text('Register'),
+                        onTap: () => _handleNavigation(5),
+                      ),
+                    ],
+                    if (_isLoggedIn != null && _isLoggedIn == true) ...[
+                      ListTile(
+                        leading: const Icon(Icons.logout_sharp),
+                        title: const Text('Logout'),
+                        onTap: () => _handleNavigation(3),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
       body: Row(
         children: [
           if (isWide)
             NavigationRail(
               selectedIndex: _selectedIndex,
               onDestinationSelected: _handleNavigation,
-              destinations: destinations,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.dashboard_customize_sharp),
+                  label: Text('Dashboard'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.person),
+                  label: Text('Profile'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                if (_isLoggedIn != null && _isLoggedIn == true) ...[
+                  NavigationRailDestination(
+                    icon: Icon(Icons.logout_sharp),
+                    label: Text('Logout'),
+                  ),
+                ],
+                if (_isLoggedIn == null || _isLoggedIn == false) ...[
+                  NavigationRailDestination(
+                    icon: Icon(Icons.login_sharp),
+                    label: Text('Login'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.app_registration_sharp),
+                    label: Text('Register'),
+                  ),
+                ],
+              ],
               labelType: NavigationRailLabelType.all,
             ),
-          const Expanded(
-            child: Center(child: Text('Select a section from the menu')),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                const Text('Settings', style: TextStyle(fontSize: 24)),
+                const SizedBox(height: 20),
+                SwitchListTile(
+                  title: Text('Clear email on logout'),
+                  subtitle: const Text('Remove saved email when you log out'),
+                  value: _clearEmailOnLogout ?? false,
+                  onChanged: (bool newValue) {
+                    setState(() {
+                      _clearEmailOnLogout = newValue;
+                    });
+                    debugPrint("_clearEmailOnLogout: $_clearEmailOnLogout");
+                    authService.value.savedEmailOnLogoutOption(
+                      _clearEmailOnLogout ?? false,
+                    );
+                  },
+                ),
+                SwitchListTile(
+                  title: const Text("Enable Fingerprint Login"),
+                  subtitle: const Text('Use biometric authentication'),
+                  value: _fingerprintEnabled,
+                  onChanged: (bool newValue) {
+                    setState(() {
+                      _fingerprintEnabled = newValue;
+                    });
+                    debugPrint("_fingerprintEnabled: $_fingerprintEnabled");
+                    authService.value.savedFingerprintLoginOption(newValue);
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
