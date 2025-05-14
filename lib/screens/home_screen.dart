6431d4b551bd04bd48423a128b820f58c84dcdfe
johnson-oragui <johnson.oragui@gmail.com>
2025-05-14 18:54:11 +0100

    feat: updated navigation based on logged in user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  bool? _isLoggedIn;
+
+  @override
+  void initState() {
+    super.initState();
+
+    authService.value.checkIsLoggedIn().then((isIn) {
+      // isLoggedIn = isIn;
+      setState(() {
+        _isLoggedIn = isIn;
+      });
+      debugPrint("during check _isLoggedIn $_isLoggedIn");
+    });
+  }
 
   Future<void> _handleNavigation(int index) async {
     // Ensure index is within valid range
@@ -27,7 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
         Navigator.pushReplacementNamed(context, '/settings');
         break;
       case 3:
-        await AuthService.logoutUser();
+        await authService.value.logoutUser();
         if (context.mounted) {
           Navigator.pushReplacementNamed(context, '/login');
         }
@@ -40,114 +54,101 @@ class _HomeScreenState extends State<HomeScreen> {
     // boolean for calculating the screen size
     final isWide = MediaQuery.of(context).size.width >= 600;
 
-    bool isLoggedIn = false;
-
-    AuthService.checkIsLoggedIn().then((isIn) {
-      isLoggedIn = isIn;
-    });
-
-    final destinations = [
-      NavigationRailDestination(
-        icon: Icon(Icons.dashboard_customize_sharp),
-        label: Text('Dashboard'),
-      ),
-      NavigationRailDestination(
-        icon: Icon(Icons.person),
-        label: Text('Profile'),
-      ),
-      NavigationRailDestination(
-        icon: Icon(Icons.settings),
-        label: Text('Settings'),
-      ),
-    ];
-
-    // not wide screen
-    List<Widget> children = [
-      const DrawerHeader(
-        decoration: BoxDecoration(color: Colors.blue),
-        margin: EdgeInsets.only(bottom: 5.0),
-        padding: EdgeInsets.only(bottom: 10.0),
-        child: Text('Menu', style: TextStyle(color: Colors.white)),
-      ),
-      ListTile(
-        leading: const Icon(Icons.dashboard_customize_sharp),
-        title: const Text('Dashboard'),
-        onTap: () => _handleNavigation(0),
-      ),
-      ListTile(
-        leading: const Icon(Icons.person),
-        title: const Text('Profile'),
-        onTap: () => _handleNavigation(1),
-      ),
-      ListTile(
-        leading: const Icon(Icons.settings),
-        title: const Text('Settings'),
-        onTap: () => _handleNavigation(2),
-      ),
-    ];
-
-    // conditionally add icons if user is logged in
-    if (isLoggedIn) {
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.logout_sharp),
-          label: Text('Logout'),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.logout_sharp),
-          title: const Text('Logout'),
-          onTap: () => _handleNavigation(3),
-        ),
-      );
-    }
-
-    if (!isLoggedIn) {
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.login_sharp),
-          label: Text('Login'),
-        ),
-      );
-      destinations.add(
-        NavigationRailDestination(
-          icon: Icon(Icons.app_registration_sharp),
-          label: Text('Register'),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.login_sharp),
-          title: const Text('Login'),
-          onTap: () => _handleNavigation(4),
-        ),
-      );
-
-      children.add(
-        ListTile(
-          leading: const Icon(Icons.app_registration_sharp),
-          title: const Text('Register'),
-          onTap: () => _handleNavigation(5),
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(title: const Text('Homescreen')),
       drawer:
           isWide
               ? null
-              : Drawer(width: 160.0, child: ListView(children: children)),
+              : Drawer(
+                width: 160.0,
+                child: ListView(
+                  children: [
+                    const DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.blue),
+                      margin: EdgeInsets.only(bottom: 5.0),
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        'Menu',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.dashboard_customize_sharp),
+                      title: const Text('Dashboard'),
+                      onTap: () => _handleNavigation(0),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text('Profile'),
+                      onTap: () => _handleNavigation(1),
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Settings'),
+                      onTap: () => _handleNavigation(2),
+                    ),
+                    if (_isLoggedIn != null && _isLoggedIn == true) ...[
+                      // conditionally add icons if user is logged in
+                      ListTile(
+                        leading: const Icon(Icons.logout_sharp),
+                        title: const Text('Logout'),
+                        onTap: () => _handleNavigation(3),
+                      ),
+                    ],
+                    if (_isLoggedIn == null || _isLoggedIn == false) ...[
+                      // conditionally add icons if user is logged in
+                      ListTile(
+                        leading: const Icon(Icons.app_registration_sharp),
+                        title: const Text('Register'),
+                        onTap: () => _handleNavigation(5),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.login_sharp),
+                        title: const Text('Login'),
+                        onTap: () => _handleNavigation(4),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
       body: Row(
         children: [
           if (isWide)
             NavigationRail(
               selectedIndex: _selectedIndex,
               onDestinationSelected: _handleNavigation,
-              destinations: destinations,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.dashboard_customize_sharp),
+                  label: Text('Dashboard'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.person),
+                  label: Text('Profile'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                ),
+                if (_isLoggedIn != null && _isLoggedIn == true) ...[
+                  // conditionally add icons if user is logged in
+                  NavigationRailDestination(
+                    icon: Icon(Icons.logout_sharp),
+                    label: Text('Logout'),
+                  ),
+                ],
+                if (_isLoggedIn == null || _isLoggedIn == false) ...[
+                  // conditionally add icons if user is logged in
+                  NavigationRailDestination(
+                    icon: Icon(Icons.login_sharp),
+                    label: Text('Login'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.app_registration_sharp),
+                    label: Text('Register'),
+                  ),
+                ],
+              ],
               labelType: NavigationRailLabelType.all,
             ),
           const Expanded(
